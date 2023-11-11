672453200a3ed686adf046ce9cbaf56f0ba1abe3
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-11-11 16:00:47 -0300

    build(issue#07): First tests created. (#8)

---
+++ todo/app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+ 
+app = FastAPI(
+  title='Todo List Manager',
+ 	version='0.1.0',
+ 	description='A simple **Todo List** manager API.'
+)
+ 
+@app.get('/')
+def hello():
+   """ texto """
+   return {'message': 'Hello, World!!!'}
