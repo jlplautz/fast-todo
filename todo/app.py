07669a2aae18a22e79850f1d1de54c8b123e00b4
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-11-12 19:57:20 -0300

    build(issue#11): Created taskipy and inserted cmds pyproject.

--- todo/app.py
+++ todo/app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
- 
+
 app = FastAPI(
-  title='Todo List Manager',
- 	version='0.1.0',
- 	description='A simple **Todo List** manager API.'
+    title='Todo List Manager',
+    version='0.1.0',
+    description='A simple **Todo List** manager API.',
 )
- 
+
+
 @app.get('/')
 def hello():
-   """ texto """
-   return {'message': 'Hello, World!!!'}
+    """texto"""
+    return {'message': 'Hello, World!!!'}
